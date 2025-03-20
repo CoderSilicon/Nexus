b5241faca4a5e741e7449e42af0864e99f53207c
Codersilicon <codersilicon@blackphantom>
2025-03-20 21:13:55 +0530

    o1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,19 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable all rules by setting them to "off" or 0
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "react-hooks/rules-of-hooks": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "react/no-unescaped-entities": "off",
+      "react/display-name": "off",
+      // You can add more rules to disable here
+    },
+  },
 ];
 
 export default eslintConfig;
